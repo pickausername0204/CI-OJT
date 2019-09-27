7c931609c0f6e7bda057ae158337720017688a3c
pickausername0204 <51364133+pickausername0204@users.noreply.github.com>
2019-09-27 11:10:09 +0900

    Fix this file
    Added instructions on how to use this code.

--- sentimental_lambda.py
+++ sentimental_lambda.py
@@ -1,13 +1,18 @@
+"""TECH-4098 [追加課題] Lambdaの利用その2(S3/SQS/Comprehend)でLambdaに書いたコードです。
+本システムでは tech-kadai-comprehend-sent-inputバケットにファイルをアップロードすると裏でSQS→lambda→Amazon comprehendがうごいて
+tech-kadai-comprehend-sent-outputバケットに感情分析された結果のファイルが出力される内容となっています。
+なお、最初にアプロードするファイルはJSONファイルを想定しています。
+"""
 import boto3,json
 
-def lambda_handler(event, context):#main function
-    target = get_file(event)#Tuple type
+def lambda_handler(event, context):#メイン関数
+    target = get_file(event)#返り値がタプル型
     target = list(target)
     target[0] = sentimental_analysis(target[0])
     upload_file(target[0],target[1])
     return 
 
-def get_file(lambda_event):#Function to get a target file from s3
+def get_file(lambda_event):#s3から目的のファイルの中身と名前を抽出する関数
     s3 = boto3.client("s3")
     bucket_name = "tech-kadai-comprehend-sent-input"
     lambda_event = json.loads(lambda_event['Records'][0]['body'])
@@ -16,12 +21,12 @@ def get_file(lambda_event):#Function to get a target file from s3
     paragraph = str(file['Body'].read())
     return paragraph,object_name
     
-def sentimental_analysis(text):#Function for emotion analysis using Amazon comprehend
+def sentimental_analysis(text):#Amazon comprehendを呼び出して、感情分析をする関数
     comprehend = boto3.client("comprehend")
     response = comprehend.detect_sentiment(Text = text,LanguageCode = "en")
     return(response)
 
-def upload_file(text_analyzed,upload_file_name):#Function to upload a target file to s3
+def upload_file(text_analyzed,upload_file_name):#s3に目的のファイルをアップロードする関数
     s3 = boto3.resource("s3")
     bucket = "tech-kadai-comprehend-sent-output"
     key = upload_file_name
